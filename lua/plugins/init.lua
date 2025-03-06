66584388922249815828c27d1606ccb96301af11
Darin Huang <huangdarin465@gmail.com>
2025-03-06 14:59:33 +1100

    Initial commit

---
+++ lua/plugins/init.lua
@@ -0,0 +1,13 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.api.nvim_create_autocmd("InsertEnter", {command = ":set norelativenumber"})
+vim.api.nvim_create_autocmd("InsertLeave", {command = ":set relativenumber"})
+
+vim.o.shiftwidth = 4
+vim.o.autoindent = true
+
+vim.keymap.set('i', 'jk', '<Esc>')
+
+return {
+  -- { "folke/lazy.nvim", version = "*" },
+}
